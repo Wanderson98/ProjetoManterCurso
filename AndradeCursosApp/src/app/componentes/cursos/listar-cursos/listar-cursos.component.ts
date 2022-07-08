7aef7cf41414601ea5c36c04dceaeb4d98a81415
Wanderson Andrade <wmandrade81@gmail.com>
2022-07-08 16:39:16 -0300

    Inicio implementação de busca por periodo

--- AndradeCursosApp/src/app/componentes/cursos/listar-cursos/listar-cursos.component.ts
+++ AndradeCursosApp/src/app/componentes/cursos/listar-cursos/listar-cursos.component.ts
@@ -9,13 +9,16 @@ import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-listar-cursos',
   templateUrl: './listar-cursos.component.html',
-  styleUrls: ['./listar-cursos.component.css']
+  styleUrls: ['./listar-cursos.component.css'],
 })
-export class ListarCursosComponent implements OnInit {
 
-  constructor(private cursoService: CursosService, private categoriaService : CategoriasService, 
-    private formBuilder: FormBuilder, private toastr: ToastrService
-    ) { }
+export class ListarCursosComponent implements OnInit {
+  constructor(
+    private cursoService: CursosService,
+    private categoriaService: CategoriasService,
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService
+  ) {}
 
   cursosAtivos!: Curso[];
   cursosFiltradosDescricao!: Curso[];
@@ -24,149 +27,172 @@ export class ListarCursosComponent implements OnInit {
   tituloModal!: string;
   idExcluir!: number;
   dataAtual: any = new Date();
+  dataInicialFiltro: any;
+  dataFinalFiltro: any;
   private _filtroCursosDescricao!: string;
 
-
-
   ngOnInit(): void {
+    
     this.CarregarCursosAtivos();
     this.CarregarCategorias();
-    this.tituloModal = "Adicionar curso";
+    this.tituloModal = 'Adicionar curso';
     this.cursoForm = this.formBuilder.group({
-      cursoId : [0],
+      cursoId: [0],
       cursoDescricao: ['', Validators.compose([Validators.required])],
       cursoDataInicial: ['', Validators.compose([Validators.required])],
       cursoDataFinal: ['', Validators.compose([Validators.required])],
       cursoQuantidadeAlunos: [0],
       isAtivo: [true],
-      categoriaId: ['', Validators.required]
+      categoriaId: ['', Validators.required],
     });
-   
   }
 
-  public get filtroListaDescricao(){
-    return this._filtroCursosDescricao
+  public get filtroListaDescricao() {
+    return this._filtroCursosDescricao;
   }
 
-  public set filtroListaDescricao(value: string){
-   this._filtroCursosDescricao = value;
-   this.cursosFiltradosDescricao = this.filtroListaDescricao ? this.filtrarCursosDescricao(this.filtroListaDescricao) : this.cursosAtivos;
- }
+  public set filtroListaDescricao(value: string) {
+    this._filtroCursosDescricao = value;
+    this.cursosFiltradosDescricao = this.filtroListaDescricao
+      ? this.filtrarCursosDescricao(this.filtroListaDescricao)
+      : this.cursosAtivos;
+  }
 
- filtrarCursosDescricao(filtro: string): any{
-  filtro = filtro.toLocaleLowerCase();
-   return this.cursosAtivos.filter(
-     (cursos: {cursoDescricao:string;}) => cursos.cursoDescricao.toLocaleLowerCase().indexOf(filtro)!== -1 
+  filtrarCursosDescricao(filtro: string): any {
+    filtro = filtro.toLocaleLowerCase();
+    return this.cursosAtivos.filter(
+      (cursos: { cursoDescricao: string }) =>
+        cursos.cursoDescricao.toLocaleLowerCase().indexOf(filtro) !== -1
     );
- }
+  }
 
-  CarregarCursosAtivos(): void
-  {
-    this.cursoService.ListarTodosCursosAtivos().subscribe( data =>{
+  CarregarCursosAtivos(): void {
+    this.cursoService.ListarTodosCursosAtivos().subscribe((data) => {
       this.cursosAtivos = data;
       this.cursosFiltradosDescricao = data;
-     
-    })
+    });
   }
 
-  LimparFormulario(): void
-  {
+  LimparFormulario(): void {
     this.cursoForm.reset();
     this.idExcluir = 0;
   }
 
-  CarregarCategorias():void
-  {
-    this.categoriaService.ListarTodasCategorias().subscribe(data =>{
+  CarregarCategorias(): void {
+    this.categoriaService.ListarTodasCategorias().subscribe((data) => {
       this.categorias = data;
-      
-    })
+    });
   }
 
-  CarregarModalEditar(item :any){
-    
-    this.tituloModal = "Editar Curso";
+  CarregarModalEditar(item: any) {
+    this.tituloModal = 'Editar Curso';
     this.cursoForm.controls['cursoId'].setValue(item.cursoId);
     this.cursoForm.controls['cursoDescricao'].setValue(item.cursoDescricao);
     this.cursoForm.controls['cursoDataInicial'].setValue(item.cursoDataInicial);
     this.cursoForm.controls['cursoDataFinal'].setValue(item.cursoDataFinal);
     this.cursoForm.controls['isAtivo'].setValue(item.isAtivo);
-    this.cursoForm.controls['cursoQuantidadeAlunos'].setValue(item.cursoQuantidadeAlunos);
+    this.cursoForm.controls['cursoQuantidadeAlunos'].setValue(
+      item.cursoQuantidadeAlunos
+    );
     this.cursoForm.controls['categoriaId'].setValue(item.categoriaId);
-
   }
 
-  EnviarFormulario(){
-    const curso : Curso = this.cursoForm.value;
-   
-    if(curso.cursoId< 1)
-    {
-      this.cursoService.AdicionarCurso(curso).subscribe(
-        {
-          next:(res) => {         
-            this.toastr.success('Curso Inserido com Sucesso!', 'Gravando!');
-            this.cursoForm.reset();
-            this.CarregarCursosAtivos();
-          },
-          error:(res)=> {
-            console.log(res)
-            if(res.error.errors  || res.error == null ){
-              this.toastr.error('Erro Inesperado', 'Error')
-            }else{
-              this.toastr.error(res.error, 'Error')
-            }
-          }
-        }   
-      );
-    } else 
-    {
-      this.cursoService.AtualizarCurso(curso).subscribe(
-        {
-          next:(res) => {         
-            this.toastr.success('Curso Atualizado com Sucesso!', 'Atualizando!');
-            this.cursoForm.reset();
-            this.CarregarCursosAtivos();
-          },
-          error:(res)=> {
-           
-            if(res.error.errors  || res.error == null ){
-              this.toastr.error('Erro Inesperado', 'Error')
-            }else{
-              this.toastr.error(res.error, 'Error')
-            }
-          }
-        }
-      )
-    }   
-  }
+  EnviarFormulario() {
+    const curso: Curso = this.cursoForm.value;
 
-  ExcluirCurso(cursoId:number)
-  {
-    this.cursoService.ExcluirCurso(cursoId).subscribe(
-      {
-        next:(res) => {         
-          this.toastr.warning('Curso Excluido com Sucesso!', 'Excluindo!');
+    if (curso.cursoId < 1) {
+      this.cursoService.AdicionarCurso(curso).subscribe({
+        next: (res) => {
+          this.toastr.success('Curso Inserido com Sucesso!', 'Gravando!');
           this.cursoForm.reset();
           this.CarregarCursosAtivos();
         },
-        error:(res)=> {
-         
-          if(res.error.errors || res.error == null ){
-            this.toastr.error('Erro Inesperado', 'Error')
-          }else{
-            this.toastr.error(res.error, 'Error')
+        error: (res) => {
+          console.log(res);
+          if (res.error.errors || res.error == null) {
+            this.toastr.error('Erro Inesperado', 'Error');
+          } else {
+            this.toastr.error(res.error, 'Error');
           }
+        },
+      });
+    } else {
+      this.cursoService.AtualizarCurso(curso).subscribe({
+        next: (res) => {
+          this.toastr.success('Curso Atualizado com Sucesso!', 'Atualizando!');
+          this.cursoForm.reset();
+          this.CarregarCursosAtivos();
+        },
+        error: (res) => {
+          if (res.error.errors || res.error == null) {
+            this.toastr.error('Erro Inesperado', 'Error');
+          } else {
+            this.toastr.error(res.error, 'Error');
+          }
+        },
+      });
+    }
+  }
+
+  ExcluirCurso(cursoId: number) {
+    this.cursoService.ExcluirCurso(cursoId).subscribe({
+      next: (res) => {
+        this.toastr.warning('Curso Excluido com Sucesso!', 'Excluindo!');
+        this.cursoForm.reset();
+        this.CarregarCursosAtivos();
+      },
+      error: (res) => {
+        if (res.error.errors || res.error == null) {
+          this.toastr.error('Erro Inesperado', 'Error');
+        } else {
+          this.toastr.error(res.error, 'Error');
         }
-      }
-    )
+      },
+    });
   }
 
-  PegarIdExclusao(cursoId:number)
+  PegarIdExclusao(cursoId: number) {
+    this.idExcluir = cursoId;
+    console.log(this.idExcluir);
+  }
+
+  filtroData(){
+    console.log(this.dataInicialFiltro)
+    console.log(this.cursosFiltradosDescricao)
+
+    if(this.dataInicialFiltro > this.dataFinalFiltro ){
+      this.toastr.error('Data final não pode ser menor que a data inicial');
+    } else if(!this.dataInicialFiltro && !this.dataFinalFiltro){
+      this.CarregarCursosAtivos();
+    }   
+    else if (this.dataInicialFiltro && !this.dataFinalFiltro){
+      this.filtrarCursoDataInicial(this.dataInicialFiltro);
+    }else if (!this.dataInicialFiltro && this.dataFinalFiltro){
+      this.filtrarCursoDataFinal(this.dataFinalFiltro);
+    }else {
+      this.filtrarCursoDataInicialFinal(this.dataInicialFiltro, this.dataFinalFiltro)
+    }
+  }
+
+  filtrarCursoDataInicial(dataInicial: any) : any
   {
-    this.idExcluir = cursoId
-    console.log(this.idExcluir)
+    this.cursosFiltradosDescricao = this.cursosFiltradosDescricao.filter(result =>{
+      return result.cursoDataInicial >= dataInicial || result.cursoDataFinal >= dataInicial
+    })
   }
 
-  
-  
+  filtrarCursoDataFinal(dataFinal: any) : any
+  {
+    this.cursosFiltradosDescricao = this.cursosFiltradosDescricao.filter(result =>{
+      return result.cursoDataInicial <= dataFinal || result.cursoDataFinal <= dataFinal
+    })
+  }
+
+  filtrarCursoDataInicialFinal(dataInicial:any, dataFinal: any) : any
+  {
+    this.cursosFiltradosDescricao = this.cursosFiltradosDescricao.filter(result =>{
+      return (result.cursoDataInicial >= dataInicial || result.cursoDataFinal >= dataInicial) && (result.cursoDataInicial <= dataFinal || result.cursoDataFinal <= dataFinal)
+    })
+  }
+
 }
